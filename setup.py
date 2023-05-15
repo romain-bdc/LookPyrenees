59628a66745e65c24e9c61b7175feecd492ac299
romain-bdc <romain.buguet-de-chargere@csgroup.eu>
2023-05-15 10:56:31 +0200

    add github actions, cli and unittest

--- setup.py
+++ setup.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
+from setuptools import setup, find_packages
 
-from setuptools import setup
-from src.obs_pyr.__init__ import version
 setup(
     name='LookPyrenees',
-    version=version,
+    version='0.0.1',
     author='Romain Buguet de Chargère',
     author_email='rbuguet@gmail.com',
     description='Downloading and visualize snow condition in pyrenees',
-
     install_requires=[
         'earthpy==0.9.4',
         'eodag==2.10.0',
         'fire==0.4.0',
-        'geopandas==0.12.2',
+        'geopandas==0.9.0',
         'matplotlib==3.7.1',
-        'numpy==1.24.2',
-        'pandas==2.0.0',
+        'pandas==1.4.4',
         'python-dateutil<2.9',
         'protobuf==3.20.0',
         'rasterio==1.3.6',
         'requests<3',
-        'rioxarray==0.9.4',
+        'rioxarray==0.13.4',
         'scipy==1.9.1',
         'Shapely==2.0.1',
         'tqdm==4.64.1',
+        'urllib3==1.25.8',
         'xarray==2023.1.0',
     ],
-    # test_suite='tests',
-    # package_dir={'': 'src'},
-    # packages=find_packages('src'),
-    # entry_points={
-    #     'console_scripts': [
-    #         'worldcereal=worldcereal.__main__:main'
-    #     ]
-    # }
+    entry_points = {
+    "console_scripts": [
+        "LookPyrenees = lookpyrenees.cli:run",
+        ]
+    },
 )
+# packages=find_packages(),
+# include_package_data=True,
---
+++ src/lookpyrenees/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version
+__version__='0.0.1'
+print(sys.version_info)
